ba9b3613f84752a3d847cbc503b8d8e991c29036
Goutam Sahoo <goutam@Goutams-MacBook-Air.local>
2022-06-03 18:15:02 +0530

    Added high score logic

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.goto(0, 265)
@@ -15,13 +16,19 @@ class Scoreboard(Turtle):
         self.show_score()
 
     def show_score(self):
-        self.write(f"Score : {self.score}", move=False, align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score} High Score: {self.high_score}", move=False, align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", move=False, align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.show_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", move=False, align=ALIGN, font=FONT)
 
     def update_score(self):
         self.score += 1
-        self.clear()
         self.show_score()
